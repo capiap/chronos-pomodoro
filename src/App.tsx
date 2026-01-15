a5a28c5425f22e06b50ecdda3831e51c815e125b
Cassiano Sardeiro <capiap@hotmail.com>
2026-01-15 19:18:19 -0300

    Aula CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { Heading } from './components/Heading';
+
+import './styles/gloval.css';
+import './styles/theme.css';
+
 export function App() {
   console.log('Oi');
 
   return (
-    <div>
-      <h1>Ola Mundo (do APP)!!</h1>
+    <>
+      <Heading />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
         facere quo ad recusandae nam autem excepturi aliquam, ex velit enim
         tempora, ipsam voluptate veritatis provident officia vitae earum magnam
         minima!
       </p>
-    </div>
+    </>
   );
 }
