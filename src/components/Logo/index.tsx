78d9f868ade3c76bdd0016cc7e1e7c1a3e86e80f
Cassiano Sardeiro <capiap@hotmail.com>
2026-01-30 23:44:22 -0300

    Logo atualizado

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,12 +1,13 @@
 import styles from './styles.module.css';
 import { TimerIcon } from 'lucide-react';
+
 export function Logo() {
   return (
-<div className={styles.logo}>
-  <a className={styles['logo-link']} href='#'>
-    <TimerIcon />
-    <span>Chronus</span>
-  </a>
-</div>
+    <div className={styles.logo}>
+      <a className={styles.logoLink} href='#'>
+        <TimerIcon />
+        <span>Chronus</span>
+      </a>
+    </div>
   );
 }
