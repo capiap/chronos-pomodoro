904a93178f73d0c80072b2e030bd5f342e14d3a9
Cassiano Sardeiro <capiap@hotmail.com>
2026-02-13 20:56:33 -0300

    home pronta

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,8 @@
-import { PlayCircleIcon } from 'lucide-react';
-import { Container } from './components/Container';
-import { Countdown } from './components/CountDown';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { DefaultInput } from './components/DefaultInput';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
 
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <Countdown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              labelText='Task'
-              id='meuInput'
-              type='text'
-              placeholder='Digite o nome da task'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,23 +1,78 @@
+import { useState, useEffect } from 'react';
 import styles from './styles.module.css';
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+    return storageTheme;
+  });
+
+  const nextIconTheme = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+
+  function handleThemeChange(
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    e.preventDefault();
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para Home'
+        title='Ir para a Home'
+      >
         <HouseIcon />
       </a>
 
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ver Historico'
+        title='Ver Historico'
+      >
         <HistoryIcon />
       </a>
 
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Configurações'
+        title='Configurações'
+      >
         <SettingsIcon />
       </a>
 
-      <a className={styles.menuLink} href='#'>
-        <SunIcon />
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Alterar Tema Claro/Escuro'
+        title='Alterar Tema Claro/Escuro'
+        onClick={handleThemeChange}
+      >
+        {nextIconTheme[theme]}
       </a>
     </nav>
   );
